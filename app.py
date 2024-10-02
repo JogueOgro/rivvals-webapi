0d7d89b78d9fd74ae517d3b8782a8c8d902285d4
Lucas Fegueredo <lucas.fegueredo@levva.io>
2024-10-02 16:56:12 -0300

    test to fix CORS

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def create_app():
     CORS(app, resources={r"/*": {
         "origins": "*",
         "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        "allow_headers": ["Content-Type", "Authorization", "X-Requested-With", 'Access-Control-Allow-Origin'],
+        "allow_headers": ["Content-Type", "Authorization", "X-Requested-With"],
         "supports_credentials": True
     }})
     
